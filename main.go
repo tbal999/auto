5caea2f4641b052e9790fda6968ebcec4f036086
tbal999 <53711814+tbal999@users.noreply.github.com>
2020-10-28 18:48:19 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"auto/pre"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,20 +9,66 @@ import (
 	"time"
 )
 
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
 func importfile(lout *os.File) {
 	filename := "instructions.auto"
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println(err.Error())
-		log.Println(err.Error())
-		return
-	}
-	instructions := strings.Split(string(content), "\n")
-	index := 0
-	for index < len(instructions) {
-		row := strings.Split(instructions[index], " ")
-		index = pre.Process(row, index, lout)
-		index++
+	if fileExists(filename) {
+		content, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		instructions := strings.Split(string(content), "\n")
+		index := 0
+		for index < len(instructions) {
+			row := strings.Split(instructions[index], " ")
+			if len(row) == 1 && row[0] == "" {
+				//
+			} else {
+				index = pre.Process(row, index, lout)
+			}
+			index++
+		}
+		log.Println("Complete")
+	} else {
+		newfile, _ := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0640)
+		defer newfile.Close()
+		guide := `>
+'AUTO' INSTRUCTIONS
+You can place multi line comments in between '>' characters. 
+You can place a # to do single line comments.
+
+Here are the commands for this application:
+
+pause - pauses the app asking for user input
+run filepath args - runs an application at a specific location i.e - "run C:\hello.exe argument1 argument2 argument3"
+deletefile filepath - deletes a file at a location i.e - "deletefile C:\hello.exe"
+copyfile sourcepath destinationpath - copies a file to a new location i.e - "copyfile C:\hello.exe C:\newhello.exe"
+clearfolder sourcepath - clears a folder of all files in it i.e - "clearfolder C:\folder"
+copyfolder sourcepath destinationpath - copies all files in a folder to new location i.e "copyfolder C:\folder C:\newfolder"
+
+You can start a loop for so many iterations with a colon i.e - ":loop 2" (loop twice)
+You can return to the loop with a goto i.e "goto :loop"
+
+Every time you run this, a log will be generated in a folder 'autologs' alongside where the application is saved.
+>
+
+####################################  Enter instructions below ###################################################
+
+
+`
+		_, err := newfile.WriteString(guide)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
 	}
 }
 
@@ -50,5 +95,4 @@ func main() {
 	}
 	log.SetOutput(logf)
 	importfile(logf)
-	log.Println("Complete")
 }
